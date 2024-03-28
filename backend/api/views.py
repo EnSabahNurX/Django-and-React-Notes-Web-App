7f18524d94ee3f4579276748603fff4d290266ab
EnSabahNurX <ricklimadealmeida@gmail.com>
2024-03-28 21:23:53 +0000

    View to create a new user

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,4 +4,9 @@ from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
+
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = User
+    permission_classes = [AllowAny]
