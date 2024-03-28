0f2b391f67ef7b5a57c367575a125562717320e4
EnSabahNurX <ricklimadealmeida@gmail.com>
2024-03-28 21:29:47 +0000

    Connecting our auth routes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,4 +5,5 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/user/register/", CreateUserView.as_view, name="register"),
 ]
