2ecf888738258694baf94ed1942751ebcf2eca56
EnSabahNurX <ricklimadealmeida@gmail.com>
2024-03-29 02:13:40 +0000

    Creating Note class

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
+
+    def __str__(self):
+        return self.title
