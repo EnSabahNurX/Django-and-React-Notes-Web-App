f98331d8ffe12f3c057b046c8fc23b202de79bfa
EnSabahNurX <ricklimadealmeida@gmail.com>
2024-03-29 23:36:10 +0000

    Implementing App.jsx to navigate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import react from 'react'
+import { BrowserRouter, Routes, Route, Navigation, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
 
