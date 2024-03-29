513bfaeb08216f5357245954aa2d57f2d7a9769f
EnSabahNurX <ricklimadealmeida@gmail.com>
2024-03-29 01:58:55 +0000

    Fixing some issues in connecting our auth routes

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,8 +5,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/user/register/", CreateUserView.as_view, name="register"),
-    path("api/token/", TokenObtainPairView.as_view, name="get_token"),
-    path("api/token/refresh/", TokenRefreshView.as_view, name="refresh"),
+    path("api/user/register/", CreateUserView.as_view(), name="register"),
+    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
 ]
