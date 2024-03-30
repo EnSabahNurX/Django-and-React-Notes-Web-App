1652318214d84c9bc782ec52082bd758a3132d92
EnSabahNurX <ricklimadealmeida@gmail.com>
2024-03-30 00:07:41 +0000

    404 Page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,8 @@
 function NotFound() {
-    return <div>NotFound</div>
+    return <div>
+        <h1>404 Not Found</h1>
+        <p>The page you're looking for doesn't exist!</p>
+    </div>
 }
 
 export default NotFound
