638d05478632a837eb3ddf03cf1e9d7f30f03a83
EnSabahNurX <ricklimadealmeida@gmail.com>
2024-03-31 00:55:09 +0000

    adding constants

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,12 @@ import { useState, useEffect } from "react"
 import api from "../api"
 
 function Home() {
+    const [notes, setNotes] = useState([]);
+    const [content, setContent] = useState("");
+    const [title, setTitle] = useState("");
+
+    
+
     return <div>Home</div>
 }
 
