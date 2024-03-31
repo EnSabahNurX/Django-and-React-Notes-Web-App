ffe294b283d4d716eb4e6dd3cdee0484cb775928
EnSabahNurX <ricklimadealmeida@gmail.com>
2024-03-31 12:24:08 +0100

    Building the Note component

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -14,3 +14,5 @@ function Note({ note, onDelete }) {
         </div>
     )
 }
+
+export default Note
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import api from "../api";
+import Note from "../components/Note"
 
 function Home() {
     const [notes, setNotes] = useState([]);
@@ -45,7 +46,7 @@ function Home() {
     return <div>
         <div>
             <h2>Notes</h2>
-
+            {notes.map((note) => <Note note={note} onDelete={deleteNote} key={note.id})} />
         </div>
         <h2>Create a Note</h2>
         <form onSubmit={createNote}>
