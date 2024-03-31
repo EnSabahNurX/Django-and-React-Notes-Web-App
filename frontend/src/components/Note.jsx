04fa02e6904f1a19394a68fd00cfe6c5709c7523
EnSabahNurX <ricklimadealmeida@gmail.com>
2024-03-31 12:15:03 +0100

    Building the Note component

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+
+function Note({ note, onDelete }) {
+    return (
+        <div className="note-container">
+            <p className="note-title">{note.title}</p>
+            <p className="note-content">{note.content}</p>
+            <p className="note-date">{ }</p>
+            <button className="delete-button" onClick={() => onDelete(note.id)}>
+                Delete
+            </button>
+        </div>
+    )
+}
