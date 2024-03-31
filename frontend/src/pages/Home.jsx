755be8cefbe5a054ebcb5c89dfb380fa0c4651a2
EnSabahNurX <ricklimadealmeida@gmail.com>
2024-03-31 00:59:44 +0000

    Building the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,20 @@
-import { useState, useEffect } from "react"
-import api from "../api"
+import { useState, useEffect } from "react";
+import api from "../api";
 
 function Home() {
     const [notes, setNotes] = useState([]);
     const [content, setContent] = useState("");
     const [title, setTitle] = useState("");
 
-    
+    const getNote = () => {
+        api
+            .get("/api/notes")
+            .then((res) => res.data)
+            .then((data) => setNotes(data))
+            .catch((err) => alert(err));
+    };
 
-    return <div>Home</div>
+    return <div>Home</div>;
 }
 
-export default Home
+export default Home;
